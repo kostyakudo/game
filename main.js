3e071baa5da6ee5f1419b49b0fdf69e314a87bfa
vatsona <valeraternavckiy1994@gmail.com>
2016-11-20 23:51:30 +0200

    second5 commit

--- main.js
+++ main.js
@@ -1,20 +1,46 @@
 $(document).ready(function(){
-    var widthBlock = '400';
-    var heightBlock = '400';
+    var widthBlock = '200';
+    var heightBlock = '200';
 
         $(".block1").css({"width":widthBlock,"height": heightBlock});
-        
+
         function ClickBlock(){
-            // var widths =  $(".block1").css.width;
-            $width = $('.block1').width();
-            $height = $('.block1').height();
+           
             $(".block1").on("click",function(){
-                $("body").addClass('body1');
+            $("body").addClass('body1');
                 var clone ;
-                $(".block1").css({"width": $width/2,"height": $height/2});
                 clone = $(".block1").clone();
+                $(clone).on("click",function(){
+                            $width = $('.block1').width();
+                            $height = $('.block1').height();
+                            var clone ;
+                            clone = $(".block1").clone();
+                                $(clone).on("click",function(){
+                                    $width = $('.block1').width();
+                                    $height = $('.block1').height();
+                                    var clone ;
+                                    clone = $(".block1").clone();
+                                       $(clone).on("click",function(){
+                                            $width = $('.block1').width();
+                                            $height = $('.block1').height();
+                                            var clone ;
+                                            clone = $(".block1").clone();
+                                                
+                                            clone.appendTo("body"); 
+                                            $(".block1").css({"width": $width/2,"height": $height/2});
+                                });       
+                                    clone.appendTo("body"); 
+                                    $(".block1").css({"width": $width/2,"height": $height/2});
+                                });      
+                            clone.appendTo("body"); 
+                            $(".block1").css({"width": $width/2,"height": $height/2});
+               });      
                 clone.appendTo("body"); 
+                 $width = $('.block1').width();
+                 $height = $('.block1').height();
+                $(".block1").css({"width": $width/2,"height": $height/2});
              });      
+
 }
         ClickBlock();
 });
